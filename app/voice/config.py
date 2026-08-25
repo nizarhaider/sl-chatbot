fc8952aa2e69e11a0a3ffdb510602853e2ef0652
Nizar Haider <nizarhaider@gmail.com>
2026-08-25 15:01:40 +0530

    move Whisper off shared 16GB GPU

--- app/voice/config.py
+++ app/voice/config.py
@@ -10,7 +10,7 @@ VLLM_TEMPERATURE = 0.2
 VLLM_PREWARM = True
 
 WHISPER_MODEL = "SPEAK-ASR/whisper-medium-si-merged"
-WHISPER_DEVICE = "cuda"
+WHISPER_DEVICE = "cpu"
 WHISPER_LANGUAGE = "sinhala"
 WHISPER_TASK = "transcribe"
 
